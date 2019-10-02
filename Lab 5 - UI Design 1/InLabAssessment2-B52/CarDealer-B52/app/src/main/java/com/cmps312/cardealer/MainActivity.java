e2e8eddbf59cabb51a781fd7429ec88b01d51367
abdalabaaji <abdala.baaji@gmail.com>
2019-10-02 15:54:39 +0300

    assign2

---
+++ Lab 5 - UI Design 1/InLabAssessment2-B52/CarDealer-B52/app/src/main/java/com/cmps312/cardealer/MainActivity.java
@@ -0,0 +1,28 @@
+package com.cmps312.cardealer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+
+/*
+
+Arabic translation
+ Best Luxury Car Dealer   United Kingdom Branch
+     أفضل تاجر سيارات فاخرة \\ فرع المملكة المتحدة
+
+  Exit
+  خروج
+
+ */
