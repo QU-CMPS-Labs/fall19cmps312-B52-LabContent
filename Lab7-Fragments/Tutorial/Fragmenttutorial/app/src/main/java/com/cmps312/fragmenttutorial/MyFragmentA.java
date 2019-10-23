1721989d9b520423f3e8a26174149b8fe5fd2e3e
abdalabaaji <abdala.baaji@gmail.com>
2019-10-23 14:49:11 +0300

    updated

--- Lab7-Fragments/Tutorial/Fragmenttutorial/app/src/main/java/com/cmps312/fragmenttutorial/MyFragmentA.java
+++ Lab7-Fragments/Tutorial/Fragmenttutorial/app/src/main/java/com/cmps312/fragmenttutorial/MyFragmentA.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -25,7 +27,20 @@ public class MyFragmentA extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_a, container, false);
+
+        TextView textView;
+
+        View rootView = inflater.inflate(R.layout.fragment_a, container, false);
+
+        textView = rootView.findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(, "I was clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
     }
 
 }
