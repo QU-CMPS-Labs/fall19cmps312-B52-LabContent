89531cddd99dcd88e3afecc78552476b620de406
abdalabaaji <abdala.baaji@gmail.com>
2019-10-23 15:03:42 +0300

    update 3:03

--- Lab7-Fragments/Tutorial/Fragmenttutorial/app/src/main/java/com/cmps312/fragmenttutorial/MyFragmentA.java
+++ Lab7-Fragments/Tutorial/Fragmenttutorial/app/src/main/java/com/cmps312/fragmenttutorial/MyFragmentA.java
@@ -2,14 +2,15 @@ package com.cmps312.fragmenttutorial;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 
 /**
@@ -29,14 +30,20 @@ public class MyFragmentA extends Fragment {
         // Inflate the layout for this fragment
 
         TextView textView;
+        final EditText inputEdt;
+        Button okBtn;
 
         View rootView = inflater.inflate(R.layout.fragment_a, container, false);
 
         textView = rootView.findViewById(R.id.textView);
-        textView.setOnClickListener(new View.OnClickListener() {
+        inputEdt = rootView.findViewById(R.id.input_edt);
+        okBtn = rootView.findViewById(R.id.ok_btn);
+
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getActivity(), "I was clicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), inputEdt.getText().toString(), Toast.LENGTH_SHORT).show();
             }
         });
 
