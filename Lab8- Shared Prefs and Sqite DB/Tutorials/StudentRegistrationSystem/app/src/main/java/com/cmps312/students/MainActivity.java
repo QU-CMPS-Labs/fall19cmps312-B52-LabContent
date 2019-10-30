4d334a175e006922230fe3c8f16ade4393c3ce9f
abdalabaaji <abdala.baaji@gmail.com>
2019-10-30 14:48:41 +0300

    update 2:48

--- Lab8- Shared Prefs and Sqite DB/Tutorials/StudentRegistrationSystem/app/src/main/java/com/cmps312/students/MainActivity.java
+++ Lab8- Shared Prefs and Sqite DB/Tutorials/StudentRegistrationSystem/app/src/main/java/com/cmps312/students/MainActivity.java
@@ -11,27 +11,56 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText username;
-    private EditText password;
+    private EditText usernameEdt;
+    private EditText passwordEdt;
+
+    public static final String USERNAME_KEY = "username";
+    public static final String PASWORD_KEY = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
-       username = findViewById(R.id.username);
-       password = findViewById(R.id.password);
+        usernameEdt = findViewById(R.id.username);
+        passwordEdt = findViewById(R.id.password);
+
     }
 
     public void registerUser(View view) {
 
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
+        SharedPreferences sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        editor.putString(USERNAME_KEY, usernameEdt.getText().toString().toLowerCase());
+        editor.putString(PASWORD_KEY, passwordEdt.getText().toString());
+
+        editor.commit();
+
 
     }
 
     //Read from shared preferences
     public void loginUser(View view) {
+
+        SharedPreferences sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+        String savedUserName = sharedPreferences.getString(USERNAME_KEY, "");
+        String savedPassword = sharedPreferences.getString(PASWORD_KEY, "");
+
+        if (
+                savedPassword.equals(passwordEdt.getText().toString())
+                && savedUserName.equals(usernameEdt.getText().toString().toLowerCase()
+
+
+        )) {
+
+
+            Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Failed to login", Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 }
