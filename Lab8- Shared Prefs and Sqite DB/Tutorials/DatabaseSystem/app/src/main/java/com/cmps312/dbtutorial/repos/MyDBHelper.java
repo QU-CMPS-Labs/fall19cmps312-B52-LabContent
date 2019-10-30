a33a67f7242e0123e44c059f6772c350f647fe19
abdalabaaji <abdala.baaji@gmail.com>
2019-10-30 15:29:29 +0300

    UPDATE 3:29

--- Lab8- Shared Prefs and Sqite DB/Tutorials/DatabaseSystem/app/src/main/java/com/cmps312/dbtutorial/repos/MyDBHelper.java
+++ Lab8- Shared Prefs and Sqite DB/Tutorials/DatabaseSystem/app/src/main/java/com/cmps312/dbtutorial/repos/MyDBHelper.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import androidx.annotation.Nullable;
-
-public class MyDBHelper extends SQLiteOpenHelper {
+public class MyDBHelper extends SQLiteOpenHelper implements StudentContract {
     public static final String DB_NAME = "student.db";
     public static final int VERSION = 1;
+
+    public static final String SQL_CREATE_STUDENT_TABLE =
+            "CREATE TABLE " + StudentTable.TABLE_NAME
+                    + "(" + StudentTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + StudentTable.COLUMN_NAME_SID + " INTEGER, "
+                    + StudentTable.COLUMN_NAME_STUDENT_NAME + " TEXT );";
+
+    public static final String SQL_DROP_STUDENT_TABLE =
+            "DROP TABLE IF EXISTS " + StudentTable.TABLE_NAME;
+
 
     public MyDBHelper(Context context) {
         super(context, DB_NAME, null, VERSION);
