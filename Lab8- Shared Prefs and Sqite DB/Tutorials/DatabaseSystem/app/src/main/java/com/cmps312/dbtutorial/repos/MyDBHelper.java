0920a800b1f882aa083e5c9fa67088a73f8e574e
abdalabaaji <abdala.baaji@gmail.com>
2019-10-30 15:21:16 +0300

    update 3:21

--- Lab8- Shared Prefs and Sqite DB/Tutorials/DatabaseSystem/app/src/main/java/com/cmps312/dbtutorial/repos/MyDBHelper.java
+++ Lab8- Shared Prefs and Sqite DB/Tutorials/DatabaseSystem/app/src/main/java/com/cmps312/dbtutorial/repos/MyDBHelper.java
@@ -1,7 +1,26 @@
 package com.cmps312.dbtutorial.repos;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
 public class MyDBHelper extends SQLiteOpenHelper {
-    
+    public static final String DB_NAME = "student.db";
+    public static final int VERSION = 1;
+
+    public MyDBHelper(Context context) {
+        super(context, DB_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
