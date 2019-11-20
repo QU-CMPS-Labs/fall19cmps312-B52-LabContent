fe6ec4f97e3341950cb9c1661a65fe34926530fc
abdalabaaji <abdala.baaji@gmail.com>
2019-11-20 15:36:07 +0300

    update 3:36

---
+++ Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/myclosecontacts/adapters/MyAdapter.java
@@ -0,0 +1,62 @@
+package com.cmps312.myclosecontacts.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cmps312.myclosecontacts.R;
+import com.cmps312.myclosecontacts.models.User;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>  {
+
+    private Context context;
+    private ArrayList<User> users;
+
+    public MyAdapter(Context context, ArrayList<User> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater
+                .from(context)
+                .inflate(R.layout.raw_layout, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.email.setText(users.get(position).getEmail());
+        holder.name.setText(users.get(position).getName().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    class MyViewHolder  extends RecyclerView.ViewHolder{
+        ImageView userImage;
+        TextView name;
+        TextView email;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            userImage = itemView.findViewById(R.id.user_image);
+            name = itemView.findViewById(R.id.name_tv);
+            email = itemView.findViewById(R.id.email_tv);
+        }
+    }
+}
