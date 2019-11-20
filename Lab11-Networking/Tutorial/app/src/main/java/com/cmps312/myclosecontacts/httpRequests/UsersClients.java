680fbed13628cbd749931ef8acb5e30f456f1e76
abdalabaaji <abdala.baaji@gmail.com>
2019-11-20 14:40:33 +0300

    update 2:40

--- Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/myclosecontacts/httpRequests/UsersClients.java
+++ Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/myclosecontacts/httpRequests/UsersClients.java
@@ -2,6 +2,7 @@ package com.cmps312.myclosecontacts.httpRequests;
 
 import com.cmps312.myclosecontacts.models.Result;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -13,6 +14,6 @@ public interface UsersClients {
     //GET , PUT , POST , DELETE
 
     @GET("/api")
-    Result getUsers(@Query("results") String results,
-                    @Query("format") String format);
+    Call<Result> getUsers(@Query("results") String results,
+                          @Query("format") String format);
 }
--- Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/myclosecontacts/models/Result.java
+++ Lab11-Networking/Tutorial/app/src/main/java/com/cmps312/myclosecontacts/models/Result.java
@@ -1,4 +1,21 @@
 package com.cmps312.myclosecontacts.models;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
 public class Result {
+
+    @SerializedName("results")
+    @Expose
+    private ArrayList<User> users;
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
 }
